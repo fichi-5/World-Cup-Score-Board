830af59c70f52590362522abbf5770c98e8df991
Federico Martín <f.martin.medina@gmail.com>
2021-02-14 12:35:41 +0100

    Delete files

---
+++ src/AddMatchForm.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+class AddMatchForm extends React.Component {
+    home_teamRef = React.createRef();
+    away_teamRef = React.createRef();
+
+    createMatch = (event) => {
+        event.preventDefault();
+        const match = {
+            home_team: this.home_teamRef.current.value,
+            away_team: this.away_teamRef.current.value,
+            result_home_team: 0,
+            result_away_team: 0,
+            status: 'Started'
+        };
+        if (match.home_team != "" && match.away_team != ""){
+            this.props.addMatch(match);
+            event.currentTarget.reset();
+        }
+    };
+
+    render(){
+        return (
+            <form className="match-edit" onSubmit={ this.createMatch }>
+                <input name="home_team" ref={this.home_teamRef} type="text" placeholder="Home Team" />
+                <input name="away_team" ref={this.away_teamRef} type="text" placeholder="Away Team" />
+
+                <button type="submit">+ Add Match</button>
+            </form>
+        );
+    }
+}
+
+export default AddMatchForm;
